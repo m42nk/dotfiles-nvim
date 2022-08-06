fae1bc39188210286019627f3433268cac63c4d1
Syakhisk Al-azmi <mail.syakhisk@gmail.com>
2022-08-07 00:49:34 +0700

    Remove diagnostics icons from bufferline

--- plugin/bufferline.lua
+++ plugin/bufferline.lua
@@ -6,9 +6,9 @@ end
 bufferline.setup {
   options = {
     diagnostics = "nvim_lsp",
-    diagnostics_indicator = function(count, level, diagnostics_dict, context)
-      local icon = level:match "error" and " " or " "
-      return " " .. icon .. count
-    end,
+    -- diagnostics_indicator = function(count, level, context)
+    --   local icon = level:match "error" and " " or " "
+    --   return icon
+    -- end,
   },
 }
