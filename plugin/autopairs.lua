a1aaf8c22f5a85d91a9fe3b9ba92e9772b64f39b
Syakhisk Al-azmi <mail.syakhisk@gmail.com>
2022-08-06 19:13:25 +0700

    Add fastwrap to autopairs

--- plugin/autopairs.lua
+++ plugin/autopairs.lua
@@ -5,4 +5,14 @@ end
 
 autopairs.setup {
   disable_filetype = { "TelescopePrompt" },
+  fast_wrap = {
+    map = "<M-e>",
+    chars = { "{", "[", "(", '"', "'" },
+    pattern = [=[[%'%"%)%>%]%)%}%,]]=],
+    end_key = "$",
+    keys = "qwertyuiopzxcvbnmasdfghjkl",
+    check_comma = true,
+    highlight = "Search",
+    highlight_grey = "Comment",
+  },
 }
