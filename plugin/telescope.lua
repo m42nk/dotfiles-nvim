732f5a504376d620222befdf0dddf7dcdbe160e4
Syakhisk Al-azmi <mail.syakhisk@gmail.com>
2022-08-08 04:12:42 +0700

    Trying to add telescope custom pickers

--- plugin/telescope.lua
+++ plugin/telescope.lua
@@ -10,3 +10,28 @@ telescope.setup {
     },
   },
 }
+
+-- local pickers = require "telescope.pickers"
+-- local finders = require "telescope.finders"
+-- local conf = require("telescope.config").values
+-- --[[
+-- Pickers => telescope window to pick something
+-- Finders => provider for list to be picked
+-- Sorter => as the name suggest
+-- --]]
+
+-- -- our picker function: colors
+-- local colors = function(opts)
+--   opts = opts or {}
+--   pickers.new(opts, {
+--     prompt_title = "colors",
+--     finder = finders.new_table {
+--       results = { "red", "green", "blue" }
+--     },
+--     sorter = conf.generic_sorter(opts),
+--   }):find()
+-- end
+
+-- -- to execute the function
+-- -- colors()
+-- colors(require("telescope.themes").get_dropdown{})
