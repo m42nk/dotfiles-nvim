0a58b463bef46cb499db786b4c38c0af53f19af7
Syakhisk Al-azmi <mail.syakhisk@gmail.com>
2022-08-08 07:06:40 +0700

    Tssssserver and reenable incremental selection (useful for json)

---
+++ lua/user/lsp/settings/tsserver.lua
@@ -0,0 +1,5 @@
+local opts = {
+	_disable_formatting = true,
+}
+
+return opts
--- lua/user/treesitter/init.lua
+++ lua/user/treesitter/init.lua
@@ -30,15 +30,15 @@ configs.setup {
       ["i;"] = "textsubjects-container-inner",
     },
   },
-  -- incremental_selection = {
-  --   enable = true,
-  --   keymaps = {
-  --     init_selection = "<CR>",
-  --     scope_incremental = "<CR>",
-  --     node_incremental = "<TAB>",
-  --     node_decremental = "<S-TAB>",
-  --   },
-  -- },
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = "<CR>",
+      scope_incremental = "<CR>",
+      node_incremental = "<TAB>",
+      node_decremental = "<S-TAB>",
+    },
+  },
   textobjects = {
     select = {
       enable = true,
