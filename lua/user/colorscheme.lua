31c3d9506c0330d5481380ac671b2a464a0a1aa7
Syakhisk Al-azmi <mail.syakhisk@gmail.com>
2022-08-09 01:41:14 +0700

    Update colorscheme

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -3,7 +3,12 @@
 ----------------------------------
 
 -- local colorscheme = "tokyonight"
+
 local colorscheme = "tokyodark"
+vim.g.tokyodark_transparent_background = false
+vim.g.tokyodark_enable_italic_comment = true
+vim.g.tokyodark_enable_italic = true
+vim.g.tokyodark_color_gamma = "1.0"
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
