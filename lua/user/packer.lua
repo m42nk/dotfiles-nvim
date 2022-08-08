ccb3b05b0e50327e22dd7b2a69297d141ff1c862
Syakhisk Al-azmi <mail.syakhisk@gmail.com>
2022-08-08 08:45:06 +0700

    Reformat packer

--- lua/user/packer.lua
+++ lua/user/packer.lua
@@ -1,41 +1,44 @@
 local fn = vim.fn
-local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
 if fn.empty(fn.glob(install_path)) > 0 then
-  packer_bootstrap = fn.system({
-    'git',
-    'clone',
-    '--depth',
-    '1',
-    'https://github.com/wbthomason/packer.nvim',
-    install_path
-  })
+  packer_bootstrap = fn.system {
+    "git",
+    "clone",
+    "--depth",
+    "1",
+    "https://github.com/wbthomason/packer.nvim",
+    install_path,
+  }
   vim.cmd [[packadd packer.nvim]]
 end
 
-vim.cmd([[
+vim.cmd [[
   augroup packer_user_config
     autocmd!
     autocmd BufWritePost plugins.lua source <afile>:p:h/packer.lua | PackerCompile
   augroup end
-]])
+]]
 
 -- Use a protected call so we don't error out on first use
 local packer_ok, packer = pcall(require, "packer")
 if not packer_ok then
-	return
+  return
 end
 
 -- Have packer use a popup window
-packer.init({
-	display = {
-		open_fn = function()
-			return require("packer.util").float({ border = "rounded" })
-		end,
-	},
-})
+packer.init {
+  -- git = {
+  --   timeout = 120,
+  -- },
+  display = {
+    open_fn = function()
+      return require("packer.util").float { border = "rounded" }
+    end,
+  },
+}
 
 return packer.startup(function(use)
-  package.loaded['user.plugins'] = nil
+  package.loaded["user.plugins"] = nil
 
   local _, plugins = pcall(require, "user.plugins")
   for _, plugin in pairs(plugins) do
@@ -45,6 +48,6 @@ return packer.startup(function(use)
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if packer_bootstrap then
-    require('packer').sync()
+    require("packer").sync()
   end
 end)
