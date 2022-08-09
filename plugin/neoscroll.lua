ec1e771c8c46de50e36cae876fc180ee39d494b3
Syakhisk Al-azmi <mail.syakhisk@gmail.com>
2022-08-09 17:11:22 +0700

    Change scroll distance with <c-u> and <c-d>

--- plugin/neoscroll.lua
+++ plugin/neoscroll.lua
@@ -9,12 +9,18 @@ neoscroll.setup {
 
 local t = {}
 -- Syntax: t[keys] = {function, {function arguments}}
-t['<C-u>'] = {'scroll', {'-vim.wo.scroll', 'true', '100'}}
-t['<C-d>'] = {'scroll', { 'vim.wo.scroll', 'true', '100'}}
+-- t['<C-u>'] = {'scroll', {'-vim.wo.scroll', 'true', '100'}}
+-- t['<C-d>'] = {'scroll', { 'vim.wo.scroll', 'true', '100'}}
+
+t['<C-u>'] = {'scroll', {-10, 'true', '100'}}
+t['<C-d>'] = {'scroll', { 10, 'true', '100'}}
+
 t['<C-b>'] = {'scroll', {'-vim.api.nvim_win_get_height(0)', 'true', '100'}}
 t['<C-f>'] = {'scroll', { 'vim.api.nvim_win_get_height(0)', 'true', '100'}}
+
 t['<C-y>'] = {'scroll', {'-0.10', 'false', '100'}}
 t['<C-e>'] = {'scroll', { '0.10', 'false', '100'}}
+
 t['zt']    = {'zt', {'100'}}
 t['zz']    = {'zz', {'100'}}
 t['zb']    = {'zb', {'100'}}
