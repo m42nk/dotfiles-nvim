2aff4dd3a12eb3acc64d4de1b05154d13e96e64c
Syakhisk Al-Azmi <63556086+Syakhisk@users.noreply.github.com>
2022-10-13 19:17:33 +0700

    remove bak

--- bak/lua/user/lsp/settings/tsserver.lua
+++
@@ -1,5 +0,0 @@
-local opts = {
-	_disable_formatting = true,
-}
-
-return opts
--- bak/plugin/carbon.lua
+++
@@ -1,8 +0,0 @@
-local ok, carbon = pcall(require, "carbon")
-if not ok then
-  return
-end
-
-carbon.setup {
-  setting = "value",
-}
