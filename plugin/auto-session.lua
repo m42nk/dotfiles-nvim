903e495ccee0acdab1a1707129ce52ba902426d6
Syakhisk Al-Azmi <63556086+Syakhisk@users.noreply.github.com>
2022-10-17 13:06:38 +0700

    update

--- plugin/auto-session.lua
+++ plugin/auto-session.lua
@@ -1,4 +1,4 @@
-local ok, autosession = pcall(require, "autosession")
+local ok, autosession = pcall(require, "auto-session")
 if not ok then
   return
 end
@@ -6,4 +6,11 @@ end
 autosession.setup {
   log_level = "error",
   auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
+}
+
+require("user.utils.keymaps").nmap {
+  ["<leader>S"] = {
+    "<cmd>Telescope session-lens search_session<cr>",
+    "Search session",
+  },
 }
