7f83064a15328ec709dddadb7bae72a4a193b862
Syakhisk Al Azmi <mail.syakhisk@gmail.com>
2023-10-30 00:14:36 +0700

    Refactor: October 2023

--- lua/user/setups/luasnip.lua
+++
@@ -1,9 +0,0 @@
--- UNUSED, called in cmp init
-local ok, _ = pcall(require, "luasnip")
-
-if not ok then
-  return
-end
-
-require("luasnip/loaders/from_vscode").lazy_load()
-require("luasnip/loaders/from_lua").lazy_load()
