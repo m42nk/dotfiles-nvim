f6f7edd79083effc81aa7bc8911963883476062d
Syakhisk Al Azmi <mail.syakhisk@gmail.com>
2023-11-27 15:26:45 +0700

    update

--- lua/ftplugin/go.lua
+++ lua/ftplugin/go.lua
@@ -1,1 +1,2 @@
-vim.keymap.set("n", "<leader>ci", "<cmd>GoIfErr<cr>", { desc = "Golang If Err", silent = false })
+vim.keymap.set("n", "<leader>cci", "<cmd>GoIfErr<cr>", { desc = "Golang If Err", silent = false })
+vim.keymap.set("n", "<leader>ccf", "<cmd>GoFillStruct<cr>", { desc = "Golang Fill Struct", silent = false })
--- lua/plugins/coding/go-nvim.lua
+++ lua/plugins/coding/go-nvim.lua
@@ -1,18 +1,15 @@
 return {
   {
     "ray-x/go.nvim",
-    enabled = false,
+    enabled = true,
     dependencies = {
       "ray-x/guihua.lua",
       "neovim/nvim-lspconfig",
       "nvim-treesitter/nvim-treesitter",
     },
     config = function()
-      local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
       require("go").setup {
-        lsp_cfg = {
-          capabilities = capabilities,
-        },
+        lsp_cfg = false,
       }
     end,
     event = { "CmdlineEnter" },
