9e84d6b123982fdf9a1e61d9cbf45eed48933036
Syakhisk Al Azmi <mail.syakhisk@gmail.com>
2023-12-22 21:53:41 +0700

    update

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -10,4 +10,22 @@ vim.g.autoformat = false
 opt.colorcolumn = "80" -- add visual border for column 80
 opt.swapfile = false -- Don't use swapfile
 opt.clipboard:remove "unnamedplus" -- don't use system clipboard
+opt.splitbelow = true -- Put new windows below current
+
+--- cursor	Keep the same relative cursor position.
+--- screen	Keep the text on the same screen line.
+--- topline	Keep the topline the same.
+-- opt.splitkeep = "screen"
+opt.splitkeep = "cursor"
+opt.splitright = true -- Put new windows right of current
+opt.fillchars = {
+  foldopen = "",
+  foldclose = "",
+  -- fold = "⸱",
+  fold = " ",
+  foldsep = " ",
+  diff = "╱",
+  eob = " ",
+}
 -- opt.spelllang = "en,id"
+
--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -11,22 +11,79 @@ return {
   -- TODO: move to proper file
   {
     "neovim/nvim-lspconfig",
+    -- opts = {
+    --   inlay_hints = { enabled = true },
+    -- },
     init = function()
       local keys = require("lazyvim.plugins.lsp.keymaps").get()
       -- change a keymap
+      -- stylua: ignore
       keys[#keys + 1] = {
         "gi",
-        function()
-          require("telescope.builtin").lsp_implementations { reuse_win = true }
-        end,
+        function() require("telescope.builtin").lsp_implementations { reuse_win = false } end,
         desc = "G To Implementations",
       }
+
+      -- stylua: ignore
+      keys[#keys + 1] = {
+        "gI",
+        function() require("telescope.builtin").lsp_implementations({ jump_type = "vsplit" }) end,
+        desc = "Goto Implementation",
+      }
+
+      -- stylua: ignore
+      keys[#keys + 1] = {
+        "gd",
+        function() require("telescope.builtin").lsp_definitions({ reuse_win = false }) end,
+        desc = "Goto Definition",
+        has = "definition",
+      }
+
+      -- stylua: ignore
+      keys[#keys + 1] = {
+        "gD",
+        function() require("telescope.builtin").lsp_definitions({ jump_type = "vsplit" }) end,
+        desc = "Goto Definition (Split)",
+        has = "definition",
+      }
+
+      -- stylua: ignore
+      keys[#keys + 1] = { "gr", "<cmd>Telescope lsp_references<cr>", desc = "References" }
+
+      -- stylua: ignore
+      keys[#keys + 1] = { "gx", vim.lsp.buf.declaration, desc = "Goto Declaration" }
+      -- stylua: ignore
+      keys[#keys + 1] = { "gy", function() require("telescope.builtin").lsp_type_definitions({ reuse_win = false }) end, desc = "Goto T[y]pe Definition" }
     end,
   },
   {
     "karb94/neoscroll.nvim",
     config = function()
-      require("neoscroll").setup {}
+      require("neoscroll").setup {
+        easing_function = "cubic",
+      }
+    end,
+    init = function()
+      local Util = require "lazyvim.util"
+      Util.on_load("which-key.nvim", function()
+        local t = {}
+        -- Syntax: t[keys] = {function, {function arguments}}
+        -- Use the "sine" easing function
+        t["<C-u>"] = { "scroll", { "-vim.wo.scroll", "true", "350", [['sine']] } }
+        t["<C-d>"] = { "scroll", { "vim.wo.scroll", "true", "350", [['sine']] } }
+        -- Use the "circular" easing function
+        t["<C-b>"] = { "scroll", { "-vim.api.nvim_win_get_height(0)", "true", "500", [['circular']] } }
+        t["<C-f>"] = { "scroll", { "vim.api.nvim_win_get_height(0)", "true", "500", [['circular']] } }
+        -- Pass "nil" to disable the easing animation (constant scrolling speed)
+        t["<C-y>"] = { "scroll", { "-0.10", "false", "100", nil } }
+        t["<C-e>"] = { "scroll", { "0.10", "false", "100", nil } }
+        -- When no easing function is provided the default easing function (in this case "quadratic") will be used
+        t["zt"] = { "zt", { "300" } }
+        t["zz"] = { "zz", { "300" } }
+        t["zb"] = { "zb", { "300" } }
+
+        require("neoscroll.config").set_mappings(t)
+      end)
     end,
   },
 }
