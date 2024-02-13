a33d1adebc917fde5245f559be47bacb56078620
Syakhisk Al Azmi <mail.syakhisk@gmail.com>
2024-02-13 16:03:11 +0700

    update

--- lua/plugins/editor/gitsigns.lua
+++ lua/plugins/editor/gitsigns.lua
@@ -2,6 +2,7 @@ return {
   {
     "lewis6991/gitsigns.nvim",
     opts = {
+      current_line_blame =  true,
       on_attach = function(buffer)
         local gs = package.loaded.gitsigns
 
@@ -19,7 +20,8 @@ return {
         map("n", "<leader>ghu", gs.undo_stage_hunk, "Undo Stage Hunk")
         map("n", "<leader>ghR", gs.reset_buffer, "Reset Buffer")
         map("n", "<leader>ghp", gs.preview_hunk, "Preview Hunk")
-        map("n", "<leader>ghb", function() gs.blame_line({ full = true }) end, "Blame Line")
+        map("n", "<leader>ghb", function() gs.blame_line() end, "Blame Line")
+        map("n", "<leader>ghB", function() gs.toggle_current_line_blame() end, "Toggle Line Blame")
         map("n", "<leader>ghd", gs.diffthis, "Diff This")
         map("n", "<leader>ghD", function() gs.diffthis("~") end, "Diff This ~")
         map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", "GitSigns Select Hunk")
