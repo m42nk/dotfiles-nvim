6ef35b4598a0fbd2f67071e5fd2c88c6771c6401
Syakhisk Al Azmi <mail.syakhisk@gmail.com>
2025-01-05 03:04:50 +0700

    update libs

--- isolated/nvim/lua/lib/customutil/init.lua
+++ isolated/nvim/lua/lib/customutil/init.lua
@@ -1,4 +1,19 @@
 ---@class Customutil
 local M = {}
 
+M.log_to_file = function(...)
+  local args = { ... }
+  local str = ""
+  for _, v in ipairs(args) do
+    str = str .. vim.inspect(v) .. " "
+  end
+  str = str .. "\n"
+
+  local fp = io.open("/tmp/nvim.log", "a")
+  if fp then
+    fp:write(str)
+    fp:close()
+  end
+end
+
 return M
--- isolated/nvim/lua/lib/requireall/init.lua
+++ isolated/nvim/lua/lib/requireall/init.lua
@@ -28,7 +28,65 @@ local filepath_to_modulename = function(filepath)
   return modulename
 end
 
-local get_files = function(dirpath, level)
+local get_files_and_dirs = function(dirpath, level)
+  local files = {}
+
+  local function scan_dir(path, current_level)
+    if current_level > level then
+      return
+    end
+
+    local entries = vim.fn.readdir(path)
+    if not entries then
+      return
+    end
+
+    for _, entry in ipairs(entries) do
+      local fullpath = path .. "/" .. entry
+      local is_directory = vim.fn.isdirectory(fullpath) == 1
+      local init_lua_path = vim.fs.joinpath(fullpath, "init.lua")
+
+      if is_directory and vim.uv.fs_stat(init_lua_path) then
+        table.insert(files, fullpath)
+      elseif is_directory then
+        scan_dir(fullpath, current_level + 1)
+      else
+        table.insert(files, fullpath)
+      end
+    end
+  end
+
+  scan_dir(dirpath, 1)
+  return files
+end
+
+-- Require all modules in the same directory as the caller.
+-- Usually used in the `init.lua` file of a module
+-- to automatically load all modules in the same directory
+---@param level? number
+M.requireall = function(level)
+  local caller_paths = get_caller_paths()
+  local modules = {}
+
+  ---@type string[]
+  local files = get_files_and_dirs(caller_paths.dirpath, level or 1)
+
+  for _, file in ipairs(files) do
+    -- skip current file, after resolving symlinks
+    if vim.uv.fs_realpath(file) ~= vim.uv.fs_realpath(caller_paths.filepath) then
+      local modname = filepath_to_modulename(file)
+      local mod = require(modname)
+      table.insert(modules, mod)
+    end
+  end
+
+  return modules
+end
+
+-- Return a list of files in a directory recursively down to a certain level (depth).
+--- @param dirpath string: The directory path
+--- @param level? number: The depth level to scan
+M.get_files = function(dirpath, level)
   local files = {}
 
   local function scan_dir(path, current_level)
@@ -52,29 +110,6 @@ local get_files = function(dirpath, level)
 
   scan_dir(dirpath, 1)
   return files
-end
-
--- Require all modules in the same directory as the caller.
--- Usually used in the `init.lua` file of a module
--- to automatically load all modules in the same directory
----@param level? number
-M.requireall = function(level)
-  local caller_paths = get_caller_paths()
-  local modules = {}
-
-  ---@type string[]
-  local files = get_files(caller_paths.dirpath, level or 1)
-
-  for _, file in ipairs(files) do
-    -- skip current file, after resolving symlinks
-    if vim.uv.fs_realpath(file) ~= vim.uv.fs_realpath(caller_paths.filepath) then
-      local modname = filepath_to_modulename(file)
-      local mod = require(modname)
-      table.insert(modules, mod)
-    end
-  end
-
-  return modules
 end
 
 return M
