5ea2fa3e1aa3fd1ca12650f6c445d5c443b50285
Syakhisk Al Azmi <mail.syakhisk@gmail.com>
2025-06-19 13:41:34 +0700

    update

--- lua/plugins/editor/activity-watch.lua
+++ lua/plugins/editor/activity-watch.lua
@@ -5,4 +5,13 @@ return {
     build = ":AWStart",
     lazy = false,
   },
+  -- {
+  --   "lowitea/aw-watcher.nvim",
+  --   opts = {
+  --     aw_server = {
+  --       host = "127.0.0.1",
+  --       port = 5600,
+  --     },
+  --   },
+  -- },
 }
